aa984c78bd0e9076e552517c0b75bbe3c4091176
Bumjin Park <qkrqjawls0922@gmail.com>
2023-10-17 11:02:17 +0900

    add postRouter

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import userRouter from "./routes/user";
 import authRouter, { verifyToken } from "./routes/auth";
+import postRouter from "./routes/post";
 
 const app = express();
 const port = 3010;
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
+app.use("/post", postRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
